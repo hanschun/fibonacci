a4d56ef6905461608232ad26f1d4d9a38a57ea0a
Hans Chun <hanschun@gmail.com>
2023-05-10 12:12:38 -0700

    add memoization, separate methods

--- Fibonacci.ts
+++ Fibonacci.ts
@@ -1,22 +1,32 @@
 import { Cache } from "./Cache";
 
 export class Fibonacci {
-    cache: Cache;
+    store: Cache;
 
     constructor() {
-        this.cache = new Cache();
+        this.store = new Cache();
     }
 
     getTerm(index: number): number {
-        if (index <= 1) {
-            return index;
-        }
-        const value = this.getTerm(index -1) + this.getTerm(index - 2);
-        this.cacheValue(index, value);
+        const value = this.getValue(index);
+        this.storeValue(index, value);
         return value;
     }
 
-    cacheValue(index: number, value: number): void {
-        this.cache.setValue(index, value);
+    getValue(index: number): number {
+        let value = 0;
+        try {
+            value = this.store.getValue(index);
+        } catch {
+            if (index <= 1) {
+                return index;
+            }
+            value = this.getTerm(index -1) + this.getTerm(index - 2);
+        }
+        return value;
+    }
+
+    storeValue(index: number, value: number): void {
+        this.store.setValue(index, value);
     }
 }
